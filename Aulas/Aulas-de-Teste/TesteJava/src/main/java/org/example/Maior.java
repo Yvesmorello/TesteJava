d02c847d8e8eb2dad2aee4b9a8b14dfdf3060c2c
Yvesmorello <124888523+Yvesmorello@users.noreply.github.com>
2023-08-18 22:27:51 -0300

    Add files via upload

---
+++ Aulas/Aulas-de-Teste/TesteJava/src/main/java/org/example/Maior.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Maior {
+
+    public boolean maior(int a, int b){
+        if(a > b){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
